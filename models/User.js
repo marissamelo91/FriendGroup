6192d51ca08f268f15c8c180d170694f60ddae4a
marissamelo91 <marissa.melo91@yahoo.com>
2024-08-11 18:24:31 -0700

    Build Models

---
+++ models/User.js
@@ -0,0 +1,42 @@
+
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address'],
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.vitural("friendCount").get(function () { return this.friends.length })
+
+const User = model("User", userSchema);
+
+module.exports = User;
